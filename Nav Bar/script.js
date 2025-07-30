dacd9df54ff3297728a101ec3b207e7137257fbc
RaviGahire <ravigahire3@gmail.com>
2025-07-30 22:20:10 +0530

    navbar

---
+++ Nav Bar/script.js
@@ -0,0 +1,8 @@
+
+const menuBtn = document.querySelector('.btn');
+const navLink = document.querySelector('.nav-links');
+
+menuBtn.addEventListener('click',()=>{
+navLink.style.display ='flex'
+
+})
